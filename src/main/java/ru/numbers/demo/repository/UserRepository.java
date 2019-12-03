e58d7196def6290a2093d8256604b09e108a9f9d
WebJavaLion <lev-2222@yandex.ru>
2019-12-04 01:51:38 +0300

    update

--- src/main/java/ru/numbers/demo/repository/UserRepository.java
+++ src/main/java/ru/numbers/demo/repository/UserRepository.java
@@ -2,6 +2,7 @@ package ru.numbers.demo.repository;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,6 +21,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmailAndPassword(String email, String password);
     List<User> findUsersByNameIsContaining (String name, Sort sort);
 
+    @Modifying
     @Query("delete from User u where u.id = :id")
     void deleteUserById(@Param("id") Long id);
 
